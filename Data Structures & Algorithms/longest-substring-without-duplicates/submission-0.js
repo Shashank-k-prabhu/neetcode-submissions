ff9015e2223b17fcaec9d128a2a81557fb7537eb
Shashank K Prabhu <83872336+Shashank-k-prabhu@users.noreply.github.com>
2026-05-13 00:39:39 +0530

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+
+        const charset = new Set();
+
+        let l = 0;
+        let res = 0;
+
+        for (let r=0;r<s.length;r++){
+            while(charset.has(s[r])){
+                charset.delete(s[l]);
+                l++;
+            }
+
+            charset.add(s[r]);
+            res= Math.max(res,r-l+1);
+        }
+        return res;
+
+    }
+}
